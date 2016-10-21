a4eab2289cb9a3960db37e8402da906431aba791
tungtv <tungtv@qsoft.com.vn>
2016-10-21 16:43:20 +0700

    Finish tungtv

---
+++ giaodien/footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tungtv
+ * Date: 10/21/2016
+ * Time: 3:06 PM
+ */
---
+++ giaodien/slide.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tungtv
+ * Date: 10/21/2016
+ * Time: 3:21 PM
+ */
