223b01f866e736203612e8d3a715dc73c087d622
tungtv <tungtv@qsoft.com.vn>
2017-08-21 07:30:46 +0700

    tungtv upcode

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 require_once 'config.php';
+require_once DIR . '/common/redict.php';
+$actual_link = 'http://'.$_SERVER['HTTP_HOST'];
+$string_check=strstr($actual_link,"www");
+if($string_check!=''){
+    redict(SITE_NAME);
+}
 require_once DIR.'/controller/default/index.php';
